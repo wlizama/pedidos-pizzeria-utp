0d1e87351780d057bf9d6b0f62e002eaaeb3fa89
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2021-06-21 23:23:26 -0500

    Estructura  de archivos

---
+++ src/pedidos/pizzeria/utp/controller/PruebaController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pedidos.pizzeria.utp.controller;
+
+/**
+ *
+ * @author wilderlizama
+ */
+public class PruebaController {
+    
+}
