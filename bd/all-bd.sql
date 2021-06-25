1293eef70362adda113496cdfb87d168bd3cee0b
Jonas <jonasarcayo@gmail.com>
2021-06-24 23:11:04 -0500

    DB

--- bd/all-bd.sql
+++ bd/all-bd.sql
@@ -1,29 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `pedidos-pizzeria` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `pedidos-pizzeria`;
 USE `pedidos-pizzeria`;
--- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
---
--- Host: localhost    Database: pedidos-pizzeria
--- ------------------------------------------------------
--- Server version	8.0.25-0ubuntu0.20.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
 -- Table structure for table `Acceso`
---
 
 DROP TABLE IF EXISTS `Acceso`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Acceso` (
   `IdAcceso` int NOT NULL AUTO_INCREMENT,
   `IdFormulario` int NOT NULL,
@@ -31,8 +12,7 @@ CREATE TABLE `Acceso` (
   PRIMARY KEY (`IdAcceso`),
   KEY `Acceso_ibfk_2` (`IdRol`),
   CONSTRAINT `Acceso_ibfk_2` FOREIGN KEY (`IdRol`) REFERENCES `Roles` (`IdRol`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=6;
 
 --
 -- Dumping data for table `Acceso`
@@ -49,16 +29,15 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Cliente` (
   `IdCliente` int NOT NULL AUTO_INCREMENT,
   `IdPersona` int NOT NULL,
   PRIMARY KEY (`IdCliente`),
   KEY `R_43` (`IdPersona`),
   CONSTRAINT `Cliente_ibfk_1` FOREIGN KEY (`IdPersona`) REFERENCES `Persona` (`IdPersona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
+
 
 --
 -- Dumping data for table `Cliente`
@@ -74,8 +53,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Comprobante`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Comprobante` (
   `IdComprobante` int NOT NULL AUTO_INCREMENT,
   `numero` int NOT NULL,
@@ -92,8 +69,7 @@ CREATE TABLE `Comprobante` (
   CONSTRAINT `Comprobante_ibfk_1` FOREIGN KEY (`IdTipoComprobante`) REFERENCES `TipoComprobante` (`IdTipoComprobante`),
   CONSTRAINT `Comprobante_ibfk_2` FOREIGN KEY (`IdPedido`) REFERENCES `Pedido` (`IdPedido`),
   CONSTRAINT `Comprobante_ibfk_3` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Comprobante`
@@ -109,8 +85,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `DetalleEnvio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `DetalleEnvio` (
   `IdDetalleEnvio` int NOT NULL AUTO_INCREMENT,
   `hora_fin` time NOT NULL,
@@ -122,8 +96,7 @@ CREATE TABLE `DetalleEnvio` (
   KEY `R_23` (`IdPedido`),
   CONSTRAINT `DetalleEnvio_ibfk_1` FOREIGN KEY (`IdEnvio`) REFERENCES `Envio` (`IdEnvio`),
   CONSTRAINT `DetalleEnvio_ibfk_2` FOREIGN KEY (`IdPedido`) REFERENCES `Pedido` (`IdPedido`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `DetalleEnvio`
@@ -139,8 +112,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `DetallePedido`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `DetallePedido` (
   `IdDetallePedido` int NOT NULL AUTO_INCREMENT,
   `cantidad` int NOT NULL,
@@ -151,8 +122,7 @@ CREATE TABLE `DetallePedido` (
   KEY `R_17` (`IdPizza`),
   CONSTRAINT `DetallePedido_ibfk_1` FOREIGN KEY (`IdPedido`) REFERENCES `Pedido` (`IdPedido`),
   CONSTRAINT `DetallePedido_ibfk_2` FOREIGN KEY (`IdPizza`) REFERENCES `Pizza` (`IdPizza`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `DetallePedido`
@@ -168,8 +138,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Direccion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Direccion` (
   `IdDireccion` int NOT NULL AUTO_INCREMENT,
   `direccion` varchar(100) NOT NULL,
@@ -185,8 +153,7 @@ CREATE TABLE `Direccion` (
   CONSTRAINT `Direccion_ibfk_1` FOREIGN KEY (`IdCliente`) REFERENCES `Cliente` (`IdCliente`),
   CONSTRAINT `Direccion_ibfk_2` FOREIGN KEY (`IdDistrito`) REFERENCES `Distrito` (`IdDistrito`),
   CONSTRAINT `Direccion_ibfk_3` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Direccion`
@@ -202,15 +169,12 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Distrito`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Distrito` (
   `IdDistrito` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `cobertura` tinyint(1) NOT NULL,
   PRIMARY KEY (`IdDistrito`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Distrito`
@@ -226,8 +190,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `DocumentoIdentidad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `DocumentoIdentidad` (
   `IdDocumentoIdentidad` int NOT NULL AUTO_INCREMENT,
   `numero` varchar(15) NOT NULL,
@@ -235,8 +197,7 @@ CREATE TABLE `DocumentoIdentidad` (
   PRIMARY KEY (`IdDocumentoIdentidad`),
   KEY `R_29` (`IdTipoDocIdentidad`),
   CONSTRAINT `DocumentoIdentidad_ibfk_1` FOREIGN KEY (`IdTipoDocIdentidad`) REFERENCES `TipoDocumentoIdentidad` (`IdTipoDocIdentidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=2;
 
 --
 -- Dumping data for table `DocumentoIdentidad`
@@ -253,8 +214,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Envio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Envio` (
   `IdEnvio` int NOT NULL AUTO_INCREMENT,
   `hora_inicio` time NOT NULL,
@@ -267,8 +226,7 @@ CREATE TABLE `Envio` (
   KEY `R_35` (`IdEstado`),
   CONSTRAINT `Envio_ibfk_1` FOREIGN KEY (`IdPersona`) REFERENCES `Persona` (`IdPersona`),
   CONSTRAINT `Envio_ibfk_2` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Envio`
@@ -284,8 +242,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Estado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Estado` (
   `IdEstado` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
@@ -293,8 +249,7 @@ CREATE TABLE `Estado` (
   PRIMARY KEY (`IdEstado`),
   KEY `fk_Estado_1_idx` (`IdTipoEstado`),
   CONSTRAINT `fk_Estado_1` FOREIGN KEY (`IdTipoEstado`) REFERENCES `TipoEstado` (`IdTipoEstado`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=13;
 
 --
 -- Dumping data for table `Estado`
@@ -311,14 +266,11 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Formulario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Formulario` (
   `IdFormulario` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`IdFormulario`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=6;
 
 --
 -- Dumping data for table `Formulario`
@@ -335,8 +287,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Pedido`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Pedido` (
   `IdPedido` int NOT NULL AUTO_INCREMENT,
   `numero` int NOT NULL,
@@ -351,8 +301,7 @@ CREATE TABLE `Pedido` (
   KEY `R_26` (`IdCliente`),
   CONSTRAINT `Pedido_ibfk_1` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`),
   CONSTRAINT `Pedido_ibfk_2` FOREIGN KEY (`IdCliente`) REFERENCES `Cliente` (`IdCliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Pedido`
@@ -368,8 +317,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Persona`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Persona` (
   `IdPersona` int NOT NULL AUTO_INCREMENT,
   `nombres` varchar(100) DEFAULT NULL,
@@ -385,8 +332,7 @@ CREATE TABLE `Persona` (
   CONSTRAINT `Persona_ibfk_1` FOREIGN KEY (`IdTipoPersona`) REFERENCES `TipoPersona` (`IdTipoPersona`),
   CONSTRAINT `Persona_ibfk_2` FOREIGN KEY (`IdDocumentoIdentidad`) REFERENCES `DocumentoIdentidad` (`IdDocumentoIdentidad`),
   CONSTRAINT `Persona_ibfk_3` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=2;
 
 --
 -- Dumping data for table `Persona`
@@ -403,8 +349,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Pizza`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Pizza` (
   `IdPizza` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
@@ -419,8 +363,7 @@ CREATE TABLE `Pizza` (
   CONSTRAINT `Pizza_ibfk_1` FOREIGN KEY (`IdTipoPizza`) REFERENCES `TipoPizza` (`IdTipoPizza`),
   CONSTRAINT `Pizza_ibfk_2` FOREIGN KEY (`IdTamanho`) REFERENCES `Tamanho` (`IdTamanho`),
   CONSTRAINT `Pizza_ibfk_3` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Pizza`
@@ -436,14 +379,11 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Roles` (
   `IdRol` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`IdRol`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4;
 
 --
 -- Dumping data for table `Roles`
@@ -460,15 +400,13 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Tamanho`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Tamanho` (
   `IdTamanho` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) NOT NULL,
   `cantidadPorciones` int NOT NULL,
   PRIMARY KEY (`IdTamanho`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `Tamanho`
@@ -484,8 +422,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `TipoComprobante`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `TipoComprobante` (
   `IdTipoComprobante` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
@@ -493,8 +429,7 @@ CREATE TABLE `TipoComprobante` (
   PRIMARY KEY (`IdTipoComprobante`),
   KEY `R_39` (`IdEstado`),
   CONSTRAINT `TipoComprobante_ibfk_1` FOREIGN KEY (`IdEstado`) REFERENCES `Estado` (`IdEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `TipoComprobante`
@@ -510,15 +445,12 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `TipoDocumentoIdentidad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `TipoDocumentoIdentidad` (
   `IdTipoDocIdentidad` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) DEFAULT NULL,
   `cantidadCaracteres` int DEFAULT NULL,
   PRIMARY KEY (`IdTipoDocIdentidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=7;
 
 --
 -- Dumping data for table `TipoDocumentoIdentidad`
@@ -535,14 +467,11 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `TipoEstado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `TipoEstado` (
   `IdTipoEstado` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) NOT NULL,
   PRIMARY KEY (`IdTipoEstado`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=5;
 
 --
 -- Dumping data for table `TipoEstado`
@@ -559,14 +488,11 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `TipoPersona`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `TipoPersona` (
   `IdTipoPersona` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`IdTipoPersona`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=6;
 
 --
 -- Dumping data for table `TipoPersona`
@@ -582,16 +508,13 @@ UNLOCK TABLES;
 -- Table structure for table `TipoPizza`
 --
 
-DROP TABLE IF EXISTS `TipoPizza`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `TipoPizza` (
   `IdTipoPizza` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `descripcion` varchar(50) NOT NULL,
   PRIMARY KEY (`IdTipoPizza`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `TipoPizza`
@@ -607,8 +530,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `Usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Usuario` (
   `IdUsuario` int NOT NULL AUTO_INCREMENT,
   `nombreUsuario` varchar(50) NOT NULL,
@@ -620,8 +541,7 @@ CREATE TABLE `Usuario` (
   KEY `R_34` (`IdRol`),
   CONSTRAINT `Usuario_ibfk_1` FOREIGN KEY (`IdPersona`) REFERENCES `Persona` (`IdPersona`),
   CONSTRAINT `Usuario_ibfk_2` FOREIGN KEY (`IdRol`) REFERENCES `Roles` (`IdRol`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=2;
 
 --
 -- Dumping data for table `Usuario`
