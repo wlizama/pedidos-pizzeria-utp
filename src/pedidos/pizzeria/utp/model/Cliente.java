8be45c1e576e9f90c50c3a55d67032aaba4614a4
Jonas <jonasarcayo@gmail.com>
2021-06-24 22:27:08 -0500

    Merge to Main

--- src/pedidos/pizzeria/utp/model/Cliente.java
+++ src/pedidos/pizzeria/utp/model/Cliente.java
@@ -12,5 +12,23 @@ package pedidos.pizzeria.utp.model;
 public class Cliente extends Persona{
     
     private int idCliente;    
+
+    public Cliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Cliente(int idCliente, int IdPersona, String nombres, String apellidos, String telefono, TipoPersona tipoPersona, DocumentoIdentidad documentoIdentidad, Estado estado) {
+        super(IdPersona, nombres, apellidos, telefono, tipoPersona, documentoIdentidad, estado);
+        this.idCliente = idCliente;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    
     
 }
